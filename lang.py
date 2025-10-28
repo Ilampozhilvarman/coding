dec3921de04c879b677027a9f9870310b24f357e
Ilampozhilvarman <ilampozhilvarman@gmail.com>
2025-10-28 18:32:02 -0500

    Update lang.py
    my first edit
    day 1
    10/28/2025

--- lang.py
+++ lang.py
@@ -1,1 +1,16 @@
-code = ""
+code = "var name = str james"
+vars = {}
+def new_var(name, value):
+  vars[name] = value
+code = "n  = str hello"
+lines = code.split("\n")
+for line in lines:
+  tokens = line.split()
+  if tokens[0] == "var":
+    #var declaration 
+    if tokens[3] == "str":
+      new_var(tokens[1], str(tokens[-1])
+
+print(vars)
+      
+    
