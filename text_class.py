13ba901a131013844fda7ad505b6484159063cd2
Ilampozhilvarman <ilampozhilvarman@gmail.com>
2025-10-29 14:33:48 -0500

    Update text_class.py
    day 1
    10/29/2025

--- text_class.py
+++ text_class.py
@@ -1,1 +1,18 @@
+class Text:
+  def __init__(self, text, iterateBy="words"):
+    self.text = text
+    self.index = 0
+    self.iterateBy = "words"
 
+def find_amount(self, key):
+  return self.text.count(key)
+  
+def find_index(self, key):
+  return self.text.find(key)
+
+def __iter__(self):
+  return self
+
+def __next__(self):
+  
+    
