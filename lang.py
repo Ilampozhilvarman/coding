ef92fb595dbb5346a09c4f20a63263aff9fb3802
Ilampozhilvarman <ilampozhilvarman@gmail.com>
2025-11-07 01:01:51 +0000

    cvc

--- lang.py
+++ lang.py
@@ -1,22 +1,37 @@
-code = "num n,9"
+code = "emp n"
 vars = {}
-data_types = ["str", "num", "obj", "lst"]
+data_types = ["str", "num", "obj", "lst", "emp"]
 def new_var(name, value):
   vars[name] = value
 lines = code.split("\n")
 for line in lines:
   if line[:3] in data_types:
     type = line[:3]
+    comma_index = line.index(",")
+    name = line[4:comma_index+1]
+    value = line[comma_index+1:]
     if type == "num":
-      new_var(line[4:line.index(",")+1], int(line[line.index(",")+1:]))
+      new_var(name, int(values))
     elif type == "str":
-      new_var(line[4:line.index(",")+1], str(line[line.index(",")+1:]))
+      new_var(name, str(values))
     elif type == "obj":
-      pass
+      values = line[comma_index+2:]
+      if not value:
+        new_var(line[name], {})
+        continue
+      obj = {}
+      for value in values.split():
+        try:
+          obj.update({str(value[0]): value[2]})
+        except IndexError:
+          obj.update({str(value[0]): None})
+      new_var(name, obj)
     elif type == "lst":
-      pass
-    else:
-      ValueError(f"{token[:3]} is not a valid type")
+      new_var(name, line[comma_index+2:].split())
+    elif type == "emp"
+      new_var(name, None)
+  else:
+    ValueError(f"{token[:3]} is not a valid type")
 print(vars)
       
     
