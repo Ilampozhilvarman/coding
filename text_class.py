341352ec2e807e9d990d0bb661567217ed4c3fac
Ilampozhilvarman <ilampozhilvarman@gmail.com>
2025-11-07 15:26:32 +0000

    11/7/2025

--- text_class.py
+++ text_class.py
@@ -3,36 +3,54 @@ class Text:
     self.text = text
     self.index = 0
     self.iterateBy = "words"
-    self.changes = 0
+    self.changes = {
+      "find_amount": 0
+      "find_index": 0
+      "remove": 0
+      "delete_all": 0
+      "reverse": 0
+    }
   
-  def change(self):
-    self.change += 1
+  def change(self, change):
+    if change in self.changes:
+      self.changes[change] += 1
+    else:
+      raise Exception("Something went wrong")
 
   def find_amount(self, key):
     return self.text.count(key)
-    self.change()
+    self.change("find_amount")
     
   def find_index(self, key):
     return self.text.find(key)
-    self.change()
+    self.change("find_index")
   
   def remove(self, key):
     if not key in self.text:
       raise Exception("Key not in text")
-    self.change()
+    self.change("remove")
     
     self.text = self.text.replace(key, "")
 
-  def delete(self):
+  def delete_all(self):
     self.text = ""
-    self.change()
+    self.change("delete_all")
 
   def reverse(self):
     self.text = self.text[::-1]
-    self.change()
+    self.change("reverse")
   
   def __str__(self):
     return f"{self.text}"
+  
+  def __repr__(self):
+    if len(self.text) > 20:
+      return f"{self.text[:10]}...{self.text[-10:]}\nchanges: {self.changes}"
+
+  def delete_key(self, key, amount=1):
+    for i in range(amount):
+      self.text = self.text.replace(key, "")
+
 
 
   
